abe336a1b42b71ae952289fc4fd81e09516191ca
Tom Bauer <tbauer1898@gmail.com>
2020-07-15 21:48:29 -0500

    Still working on stretch.

--- index.js
+++ index.js
@@ -182,12 +182,17 @@ and should return the average number of words per item in the array.
 
 For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
 
-function getAverageWordLength(/*code here*/){
-
-    /*code here*/
-
+function getAverageWordLength(targetArr){
+    totalSpaces = 0;
+    for(let i = 0; i < targetArr.length; i++){
+        if(targetArr[i][i] === " "){
+            totalSpaces ++ ;
+        }
+    }
+    return totalSpaces;
 }
 
+console.log(getAverageWordLength(originalFlavors));
 
 /* STRETCH 2: Baskin Robins now offers new flavors, seasonal flavors, and even regional flavors. Write a function that will randomly select a total of 31 flavors from originalFlavors, currentFlavors, seasonalFlavors, and regionalFlavors.
 
